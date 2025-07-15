1fa515b6d0247956cafd0021d3a8f825e743150a
Jashandeep <jashandeep0673.be21@chitkara.edu.in>
2025-07-15 16:12:14 +0530

    middleware update

---
+++ middleware/db_con.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect(function(err) {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        throw err;
+    }
+    console.log('Connected to database successfully!');
+   
+});
+
+
+
+module.exports=db;
